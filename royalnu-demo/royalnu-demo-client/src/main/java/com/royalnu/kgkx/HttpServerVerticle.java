88bb39fd07cdb2809b488594e9d29aedef7f881b
jk1216 <81555166@qq.com>
2017-02-25 17:26:35 +0800

    增加demo

---
+++ royalnu-demo/royalnu-demo-client/src/main/java/com/royalnu/kgkx/HttpServerVerticle.java
@@ -0,0 +1,28 @@
+package com.royalnu.kgkx;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.ext.web.Router;
+
+public class HttpServerVerticle extends AbstractVerticle {
+	
+	@Override
+	public void start(Future<Void> fut) throws Exception {
+		int port = config().getInteger("http.port", 8080);		
+		Router router=Router.router(vertx);
+		
+		
+		
+		
+		vertx.createHttpServer().requestHandler(router::accept).listen(port,result->
+		{
+			if(result.succeeded())
+			{
+				fut.complete();
+			}else{
+				fut.fail(result.cause());
+			}
+		});
+		
+	}
+}
